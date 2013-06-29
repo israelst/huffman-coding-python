35e3d2a905f88eed7c9a72e4d5847fef5c77f1f2
Israel Teixeira <israelst@gmail.com>
2013-06-29 16:08:51 -0300

    Add exemple

---
+++ presentation.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+texto = 'banana'
+frequencia(texto)
+prefixos(frequencia(texto))
+dicionario(prefixos(frequencia(texto)))
+codificar(texto, dicionario(prefixos(frequencia(texto))))
+decodificar('001011011', prefixos(frequencia(texto)))
